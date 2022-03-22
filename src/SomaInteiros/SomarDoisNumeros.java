0602e7e50114c20dddb573da93ac5f0ce77c8d22
Gustavo Gomes <gomgustav.dev@gmail.com>
2022-03-22 20:16:27 -0300

    Update SomarDoisNumeros.java

--- src/SomaInteiros/SomarDoisNumeros.java
+++ src/SomaInteiros/SomarDoisNumeros.java
@@ -4,16 +4,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class SomarDoisNumeros {
-		public static void main(String[] Args) throws NumberFormatException, IOException {
-			BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("Digite o primeiro número:");
-			int num1 = Integer.parseInt(reader.readLine());
-			System.out.println("Digite o segundo número:");
-			int num2 = Integer.parseInt(reader.readLine());
-			int result = num1 + num2;
-			System.out.println(num1 + " + " + num2 + " = " + result);
-		}
+	public static void main(String[] Args) throws NumberFormatException, IOException {
+		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Digite o primeiro número:");
+		int num1 = Integer.parseInt(reader.readLine());
+		System.out.println("Digite o segundo número:");
+		int num2 = Integer.parseInt(reader.readLine());
+		int result = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + result);
 	}
+}
 
 
 
